ca10bb912b044b2566b7147420aa36ccd8fbd7e1
NguyenDucBien <DucBien2311@gmail.com>
2016-12-14 17:08:25 +0700

    roll

--- RollingBall/ViewController.swift
+++ RollingBall/ViewController.swift
@@ -7,68 +7,108 @@
 //
 
 import UIKit
-
+import AVFoundation
 
 class ViewController: UIViewController {
+    
     
     var ball = UIImageView()
     var radians = CGFloat()
     var ballRadious = CGFloat()
-    var run = true
-    var rdy: Double = 0.0
-
-    
+    var right: Bool = true
+    var direct: CGFloat = 1
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addBall()
-        let time = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: #selector(runningBall), userInfo: nil, repeats: true)
+        playSong()
+        
+        let time = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: #selector(changeDirection), userInfo: nil, repeats: true)
         
     }
-    func addBall(){
+    
+    func addBall()
+    {
         let mainViewSize = self.view.bounds.size
-        ball = UIImageView(image: UIImage(named: "ball.png"))
+        
+        ball = UIImageView(image: UIImage(named: "ball"))
         ballRadious = 32.0
-        ball.center = CGPointMake(0,0)
+        ball.center = CGPointMake(ballRadious,mainViewSize.height * 0.5)
         self.view.addSubview(ball)
-    }
-    
-    func runningBall(){
         
-        var deltaAngle: CGFloat = 0.1
-        
-        
-        if (run == false){   //bóng quay theo chiều ngược lại, góc quay âm
-            deltaAngle = -0.1
-        }
-        
-        if (ball.center.x + 32.0 > self.view.bounds.width || ball.center.y + 32.0 > self.view.bounds.height){  //bóng đập lại khi chạm biên phải hoặc bên dưới
-            run = false
-            ballRadious = -ballRadious
-            rdy = Double(arc4random_uniform(92) % 100) * 0.01
-
-        }
-        
-        
-        if (ball.center.x - 32.0 < 0 || ball.center.y - 32.0 < 0){  //bóng đập lại khi chạm biên trái hoặc bên trên
-            run = true
-            ballRadious = 32.0
-            rdy = Double(arc4random_uniform(92) % 100) * 0.01
-            
-        }
-        
-        radians = radians + deltaAngle
-        ball.transform = CGAffineTransformMakeRotation(radians)
-        rollBall()
         
     }
-    
     func rollBall()
     {
         var deltaAngle: CGFloat = 0.1
-       self.ball.center = CGPointMake(self.ball.center.x + ballRadious * deltaAngle, self.ball.center.y + ballRadious * deltaAngle * CGFloat(rdy))
-
+        radians = radians + deltaAngle
+        ball.transform = CGAffineTransformMakeRotation(radians)
+        
+        
+        self.ball.center = CGPointMake(self.ball.center.x + ballRadious * deltaAngle, self.ball.center.y + (ballRadious * deltaAngle) * direct )
+        
+        
+        if ball.center.x + 32 > self.view.bounds.width
+        {
+            deltaAngle = -0.1
+            right = false
+        }
     }
-
+    
+    
+    func ballRoll()
+    {
+        var deltaAngle: CGFloat = -0.1
+        radians = radians + deltaAngle
+        ball.transform = CGAffineTransformMakeRotation(radians)
+        
+        
+        self.ball.center = CGPointMake(self.ball.center.x + ballRadious * deltaAngle, self.ball.center.y - (ballRadious * deltaAngle) * direct )
+        
+        
+        if ball.center.x - 32 < 0        {
+            deltaAngle = 0.1
+            right = true
+        }
+    }
+    
+    
+    func changeDirection()
+    {
+        if right
+        {
+            changeDirect()
+            rollBall()
+        }
+        else
+        {
+            changeDirect()
+            ballRoll()
+        }
+    }
+    
+    
+    func changeDirect()
+    {
+        if ball.center.y + 32 > self.view.bounds.height
+        {
+            direct = -1
+        }
+        else if ball.center.y - 32 < 0        {
+            direct = 1
+        }
+    }
+    
+    
+    func playSong (){
+        let filePath = NSBundle.mainBundle().pathForResource("Brazil-2014", ofType: ".mp3")
+        let url = NSURL(fileURLWithPath: filePath!)
+        audioPlayer = try! AVAudioPlayer(contentsOfURL: url)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+        
+    }
+    
 }
 
